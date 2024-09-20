aaa2ec5e02c482540edcb4adde56f46b57c3e796
Het Pandya <het66pandya@gmail.com>
2024-09-20 00:17:53 -0400

    Addappoinmnet, delete appointment, sorting and searching appointment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { BiCalendar} from  'react-icons/bi';
+import { useState, useCallback, useEffect } from 'react';
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import AppointmentInfo from './components/AppointmentInfo';
 
 function App() {
+  let[appointmentList, setAppointmentList] = useState([]);
+  let[query, setQuery] =  useState("");
+  let[sortBy, setSortBy] = useState("petName");
+  let[orderBy, setOrderBy] = useState("asc");
+
+  const filteredAppointment = appointmentList.filter(
+    item => {
+      return(
+        item.petName.toLowerCase().includes(query.toLowerCase())||
+        item.ownerName.toLowerCase().includes(query.toLowerCase())||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())||
+        item.aptDate.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  ).sort((a,b) =>{
+    let order = (orderBy === 'asc') ? 1  : -1;
+    return(
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1*order : 1*order
+
+    )
+  })
+
+  const fetchdata = useCallback( () =>{
+    fetch('./data.json')
+    .then(response => response.json())
+    .then(data => 
+      {setAppointmentList(data)
+
+      });
+  },[])
+
+  useEffect( () => {
+    fetchdata();
+  },[fetchdata]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-main m-5">
+      <h1 className='text-5xl m-5'>
+        <BiCalendar className='inline-block text-red-400 align-top'/>Your Apointments</h1>
+        <AddAppointment 
+          onSendAppointment = {myAppoinment => setAppointmentList([...appointmentList,myAppoinment])}
+          lastId ={appointmentList.reduce((max,item) => Number(item.id) > max ? Number(item.id) : max, 0)}/>
+        <Search query={query}
+          onQueryChange={myQuery => setQuery(myQuery)}
+          orderBy= {orderBy}
+          onOrderByChange = {mySort => setOrderBy(mySort)}
+          sortBy = {sortBy}
+          onSortByChange = {mySort => setSortBy(mySort)}
+          />
+        <ul className='divide-y divide-gray-200 m-5'>
+          {filteredAppointment
+          .map(appointment => (
+            <AppointmentInfo key={appointment.id} 
+              appointment={appointment}
+              onDeleteAppointment={
+                appointmentId => 
+                  setAppointmentList(prevAppointment => prevAppointment.filter(appointment => appointment.id  !== appointmentId))} 
+            />
+
+          ))}
+        </ul>
     </div>
   );
 }
